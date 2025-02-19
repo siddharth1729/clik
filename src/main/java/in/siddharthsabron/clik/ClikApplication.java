e240046fcc61f4fe1be70c9f98b772c2b96c8ab3
siddharth1729 <siddharth.sabron@gmail.com>
2025-02-19 22:02:11 +0530

    DEV: Setting up the project

---
+++ src/main/java/in/siddharthsabron/clik/ClikApplication.java
@@ -0,0 +1,13 @@
+package in.siddharthsabron.clik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClikApplication.class, args);
+    }
+
+}
