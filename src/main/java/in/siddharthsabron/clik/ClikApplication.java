4297746f265bdb237caf70cd031356e1e9b5eb65
siddharth1729 <siddharth.sabron@gmail.com>
2025-02-19 22:30:36 +0530

    DEV: Adding resources and profiles

--- src/main/java/in/siddharthsabron/clik/ClikApplication.java
+++ src/main/java/in/siddharthsabron/clik/ClikApplication.java
@@ -2,8 +2,12 @@ package in.siddharthsabron.clik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({@PropertySource("classpath:application.properties"),
+                @PropertySource("classpath:/profiles/${spring.profiles.active}.application.properties")})
 public class ClikApplication {
 
     public static void main(String[] args) {
